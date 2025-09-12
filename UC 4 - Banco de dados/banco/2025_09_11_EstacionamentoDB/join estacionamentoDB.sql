802f2f30454bd77e8cbc8132846ee290fa592fe3
jose.ccna <jose.ccna@gmail.com>
2025-09-12 19:11:12 -0300

    inicio da aula 12-09-2025

--- UC 4 - Banco de dados/banco/2025_09_11_EstacionamentoDB/join estacionamentoDB.sql
+++ UC 4 - Banco de dados/banco/2025_09_11_EstacionamentoDB/join estacionamentoDB.sql
@@ -4,5 +4,6 @@ SELECT * FROM cliente;
 
 select cliente.*, veiculo.modelo, veiculo.placa from cliente
 inner join veiculo
-on cliente.id = veiculo.cliente_id;
+on cliente.id = veiculo.cliente_id
+order by cliente.nome asc;
 
