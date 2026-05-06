d5c859621f49dfb2896f51ef75d4bf967f48fd85
jose.ccna <jose.ccna@gmail.com>
2026-05-05 22:10:32 -0300

    Adiciona estilos CSS ao componente Hero e destaca "escolas técnicas"

--- UC9_WEB/React_Projeto/LandingPage/edutrack/src/components/Hero/Hero.tsx
+++ UC9_WEB/React_Projeto/LandingPage/edutrack/src/components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import Estatisticas from "./Estatisticas";
+import './Hero.css';
 
 function Hero(){
     return(
@@ -7,7 +8,7 @@ function Hero(){
 
                 <span className="badge">Plataforma de aprendizado do futuro</span>
 
-                <h1>Gestão de aprendizado inteligente para escolas técnicas</h1>
+                <h1>Gestão de aprendizado inteligente para <span className="texto-azul">escolas técnicas</span></h1>
 
                 <p className="descricao-hero">Centralize avaliações, presença e progresso dos alunos em uma interface intuitiva e potente. Desenvolvido para instituições que buscam excelência acadêmica.                    
                 </p>
