a7336747b06365f11dbea2743aff62509704e3a4
jose.ccna <jose.ccna@gmail.com>
2026-06-23 19:27:18 -0300

    Iniciado a UC11 TESTE

---
+++ UC11_TESTE/IntroducaoTestes/src/UtilitariosApp/Calculadora.cs
@@ -0,0 +1,48 @@
+﻿using System.Data;
+
+namespace UtilitariosApp
+{
+    public class Calculadora
+    {
+        public int Somar(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Multiplicar(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Dividir(double a, double b)
+        {
+            if (b == 0) return (int)a;
+
+            var resultado = a / b;
+            return (int)Math.Ceiling(resultado);
+        }
+
+        public int SubtrairPositivoOuZero(int a, int b)
+        {
+            return b > a ? 0 : a - b;
+
+        }
+
+        public double PotenciaDeUmNumero(int @base, int expoente)
+        {
+
+            return Math.Pow(@base, expoente);
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ UC11_TESTE/IntroducaoTestes/tests/UtilitariosApp.Tests/CalculadoraTests.cs
@@ -0,0 +1,68 @@
+﻿using UtilitariosApp;
+
+namespace UtilitariosApp.Tests
+{
+    
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Somar_DeveRotornarASomaCorreta()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Somar(5, 7);
+            var resultadoZerado = calculadora.Somar(0, 0);
+
+            Assert.Equal(12, resultado);
+            Assert.Equal(0, resultadoZerado);
+        }
+
+        [Fact]
+        public void Multiplicar_DeveRetornarOProdutoCorreto()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Multiplicar(3, 3);
+
+            Assert.Equal(9, resultado);
+        }
+
+        [Fact]
+        public void Dividir_DeveRetornarOResultadoInteiroDaDivisaoArredondadoParaCima()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Dividir(5, 3);
+
+            Assert.Equal(2, resultado);
+        }
+
+        [Fact]
+        public void Dividir_AoDividirPorZero_DeveRetornarODividendo()
+        {
+            var calculadora = new Calculadora();
+            var resultado = calculadora.Dividir(5, 0);
+
+            Assert.Equal(5, resultado);
+        }
+        [Theory]
+        [InlineData(10, 3, 1000)]
+        [InlineData(5, 3, 125)]
+        [InlineData(5, 2, 25)]
+        [InlineData(2, -2, 0.25)]
+        [InlineData(-32, 0, 1)]
+        [InlineData(15, 1, 15)]
+        [InlineData(-15, 1, -15)]
+        public void PotenciaDeUmNumero_DeveRetrornarAPotencia(int a, int b, double pot)
+        {
+            Calculadora c1 = new Calculadora();
+            var resultado = c1.PotenciaDeUmNumero(a, b);
+
+            Assert.Equal(pot, resultado);
+
+        }
+
+
+
+
+
+
+    }
+}
