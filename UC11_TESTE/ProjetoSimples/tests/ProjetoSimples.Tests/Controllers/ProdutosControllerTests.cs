7974033771b5735fcb5639cc2da8bf5882ec1c3c
jose.ccna <jose.ccna@gmail.com>
2026-06-30 22:07:34 -0300

    Teste de unitario da aplicação

---
+++ UC11_TESTE/ProjetoSimples/tests/ProjetoSimples.Tests/Controllers/ProdutosControllerTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using ProjetoSimples.API.Controllers;
+using ProjetoSimples.API.Models;
+using ProjetoSimples.API.Services;
+
+
+namespace ProjetoSimples.Tests.Controllers
+{
+    public class ProdutosControllerTests
+    {
+        [Fact]
+        public void GetById_DeveRetornarOk_QuandoProdutoExistir()
+        {
+            // Arrange
+            // 1 Criamos um mock (imitador, dublê) da ProdutoService
+            var mockService = new Mock<IProdutoService>();
+
+            // 2 Ensinamos ao mock, como  responder chamadas por ID.
+            mockService.Setup(s =>s.ObterPorId(40)).Returns(new Produto { Id = 40, Nome = "Produto Teste", Preco = 10.0m });
+
+            //3 Instanciamos a controller, dependendo do mock, e não da service
+            var controller = new ProdutosController(mockService.Object);
+
+            // Act
+            var result = controller.GetById(40);
+
+
+            // Assert
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var produtoRetornado = Assert.IsType<Produto>(okResult.Value);
+            Assert.Equal("Produto Teste", produtoRetornado.Nome);
+            Assert.NotNull(okResult);
+        }
+
+    }
+
+
+
+
+}
---
+++ UC11_TESTE/ProjetoSimples/tests/ProjetoSimples.Tests/Services/ProdutoServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using ProjetoSimples.API.Models;
+using ProjetoSimples.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoSimples.Tests.Services
+{
+    public class ProdutoServiceTests
+    {
+        [Fact]
+        public void Adicionar_DeveInserirProdutoERetornarcomIdGerado()
+        {
+            // Arrange
+            var service = new ProdutoService();
+            var produto = new Produto { Nome = "Mouse USB", Preco = 150.00m };
+
+            // Act
+            var resultado = service.Adicionar(produto);
+
+            // Assert
+            Assert.True(resultado.Id > 0);
+            Assert.Equal("Mouse USB", resultado.Nome);
+            Assert.Single(service.ObterTodos()); // Verifica se há apenas um produto na lista
+
+        }
+
+        [Fact]
+        public void ObterPorId_DeveRetornarNulo_QuandoProdutoNaoExiste()
+        {
+            // Arrange
+            var service = new ProdutoService();
+
+            // Act
+            var resultado = service.ObterPorId(999); // ID que não existe
+
+            // Assert
+            Assert.Null(resultado);
+
+
+        }
+
+    } 
+    
+}
