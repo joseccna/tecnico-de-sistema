8302c23a9e60a33cd1fdf0a454b8688fe9f7935c
jose.ccna <jose.ccna@gmail.com>
2026-06-30 21:20:40 -0300

    Controller e mapeamento de interface

---
+++ UC11_TESTE/ProjetoSimples/src/ProjetoSimples.API/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoSimples.API.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty; // Inicializando a propriedade Nome com uma string vazia para evitar valores nulos.
+        public decimal Preco { get; set; }
+    }
+
+}
---
+++ UC11_TESTE/ProjetoSimples/src/ProjetoSimples.API/Services/ProdutoService.cs
@@ -0,0 +1,43 @@
+﻿using ProjetoSimples.API.Models;
+
+namespace ProjetoSimples.API.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+
+        private readonly List<Produto> _produtos = new(); // Lista para armazenar os produtos em memória.
+
+        private int _id = 1; // Variável para gerar IDs únicos para os produtos.
+
+
+
+        public Produto Adicionar(Produto produto)
+        {
+            produto.Id = _id++;
+            _produtos.Add(produto);
+            return produto;
+        }
+
+        public Produto? ObterPorId(int id)
+        {
+            return _produtos.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Produto> ObterTodos()
+        {
+            return _produtos;
+        }
+
+        public bool Remover(int id)
+        {
+            var produto = ObterPorId(id);
+            if (produto == null) return false;
+
+            _produtos.Remove(produto);
+            return true;
+        }
+
+    }
+
+
+}
